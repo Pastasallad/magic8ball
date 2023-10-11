3c0b57ff1d92bf05e654aef70332755ed2f73753
Pastasallad <pastasallad@gmail.com>
2023-10-11 14:06:55 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const predictions = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask<br>again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+];
+
+function getPrediction() {
+    return '<p class="w3-animate-zoom">' + predictions[Math.floor(Math.random()*predictions.length)] + '</p>';
+}
